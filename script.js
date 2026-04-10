950bd7078fdf03e283edc0ff13142e6db83b15af
obrienl8225 <z2chy67qhf@privaterelay.appleid.com>
2026-04-10 00:52:27 +0000

    added too low, too high and fixed play function

--- script.js
+++ script.js
@@ -7,34 +7,60 @@ let totalGuesses = 0;
 let scores = [];
 
 //Player Name
-let playerName = prmopt("Please enter your name:");
+let playerName = prompt("Please enter your name:");
 if (playerName === null || playerName.trim() === "") {
     playerName = "Player";
 }
 
+
 //play
 //get level
-document.getElement =ById("playBtn").addEventListener
-("click" , function() {
-    let radios = document.getElementById("level");
-    let range = 3;
-    for (let i=0; i < radios.length; i++) {
+document.getElementById("playBtn").addEventListener("click", function() {
+
+    let radios = document.getElementsByName("level");
+    let range = 3; // default
+guessCount = 0 
+    // get selected level
+    for (let i = 0; i < radios.length; i++) {
         if (radios[i].checked) {
             range = parseInt(radios[i].value);
         }
     }
-});
+
 
 //round setup
-answer= Math.floor(Math.random() * range) + 1;
-document.getElementById("msg").textContent = playerName + ", guess a number between 1 and " + range;
-document.getElementById("guess").value="";
-document.getElementById("guessBtn").disabled = false;
-document.getElementById("giveUpBtn").disabled= false;
-document.getElementById("playBtn").diabled= true;
+  answer = Math.floor(Math.random() * range) + 1;
 
-let levelRadios = document.getElementsByName("level");
-for (let i = 0; i< radios.lenth; i++) {
-    levelRadios[i].disabled = true;
-}
+    document.getElementById("msg").textContent =
+        playerName + ", guess a number between 1 and " + range;
 
+    document.getElementById("guess").value = "";
+    document.getElementById("guessBtn").disabled = false;
+    document.getElementById("giveUpBtn").disabled = false;
+    document.getElementById("playBtn").disabled = true;
+
+     for (let i = 0; i < radios.length; i++) {
+        radios[i].disabled = true;
+    }
+})
+//guess button 
+document.getElementById("guessBtn").addEventListener("click", function() {
+    let guess = parseInt(document.getElementById("guess").value);
+    guessCount++;
+
+    if (guess > answer) {
+        document.getElementById("msg").textContent = 
+        playerName + " , too high";
+    }
+    else if (guess < answer) {
+        document.getElementById("msg").textContent =
+        playerName + " , too low";
+
+    }
+    else {
+        document.getElementById("msg").textContent = 
+        playerName + " , correct";
+
+        document.getElementById("guessBtn").disabled = true
+    }
+})
